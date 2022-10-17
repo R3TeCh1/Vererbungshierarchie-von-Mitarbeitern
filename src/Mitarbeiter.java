ea45865062d10d9e941cc5c602a5686846faa571
Ma-Vollmer <matthias.vo2107@gmail.com>
2022-10-17 19:22:50 +0200

    initial abstract class

---
+++ src/Mitarbeiter.java
@@ -0,0 +1,28 @@
+public abstract class Mitarbeiter {
+    protected String vorname;
+    protected String nachname;
+    protected float jahresgehaltBisHeute;
+
+    Mitarbeiter(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public String getVorname() {
+        return nachname;
+    }
+
+    public float getJahresgehaltBisHeute() {
+        return jahresgehaltBisHeute;
+    }
+
+    public String toString() {
+        return getClass().getSimpleName() + " " + nachname + ", " + vorname;
+    }
+
+    public abstract float endgeldBerechnen();
+}
