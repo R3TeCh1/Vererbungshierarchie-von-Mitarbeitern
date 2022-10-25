596926acc61e79b4783642c352f39c18dc5d1bb7
R3TeCh_ <dadas.99@hotmail.de>
2022-10-25 22:03:03 +0200

    //Changed and added methods + constructor for ATAngestellter

--- src/ATAngstellter.java
+++ src/ATAngstellter.java
@@ -5,29 +5,57 @@ import java.util.GregorianCalendar;
  * @author Kadir Erzurum
  * @version 17.10.2022
  */
-public class ATAngstellter extends Mitarbeiter{
+public class ATAngstellter implements IMitarbeiter, ISteuerZahler{
 
     //declare
-    private float monatsLohn;
+    private String vorname;
+    private String nachname;
+    private float jahresgehaltBisHeute;
+    private float monatslohn;
+    private int gearbeiteteMonate;
 
     ATAngstellter(String vorname, String nachname, float monatsLohn){
-        super (vorname, nachname);
-        this.monatsLohn = monatsLohn;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.monatslohn = monatsLohn;
+        this.gearbeiteteMonate = new GregorianCalendar().get(Calendar.MONTH);
+        if(monatslohn/(40*4) < MINDEST_LOHN) {
+            System.out.println("Monatslohn: " + monatslohn + ", ist weniger als der Mindestlohn.");
+        }
+        System.out.println("Jahresgehalt bis heute beträgt: " + getJahresgehaltBisHeute());
     }
 
-    public void setMonatsLohn(float monatsLohn){
-        this.monatsLohn = monatsLohn;
-    }
 
     public float getMonatsLohn(){
         return monatsLohn ;
     }
 
+    public float voraussichtlicheEinkommenssteuer() {
+        return (jahresgehaltBisHeute/gearbeiteteMonate) * 12f * 0.16f;
+    }
+
+    public float tatsächlicheEinkommenssteuer() {
+        return jahresgehaltBisHeute * 0.16f;
+    }
+
+    public float getJahresgehaltBisHeute() {
+        jahresgehaltBisHeute = 0;
+        GregorianCalendar gc = new GregorianCalendar();
+        for (int months = 0; months < gc.get(Calendar.MONTH); months++) {
+            jahresgehaltBisHeute += entgeltBerechnen();
+        }
+        return jahresgehaltBisHeute;
+    }
+
     @Override
     public float entgeltBerechnen() {
         if (new GregorianCalendar().get(Calendar.MONTH) == Calendar.JANUARY) jahresgehaltBisHeute = 0;
-        super.jahresgehaltBisHeute += monatsLohn;
+        jahresgehaltBisHeute += monatsLohn;
         return monatsLohn;
+    }
+
+    public String toString() {
+        return getClass().getSimpleName() + " " + nachname + ", " + vorname;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@
  */
 public class Main {
     public static void main(String[] args) {
-        //Zeitarbeiter zeitarbeiter = new Zeitarbeiter("Nazanin", "Golalizadeh", 15.0f, 40);
-        //printAndCalculate(zeitarbeiter);
+        Zeitarbeiter zeitarbeiter = new Zeitarbeiter("Nazanin", "Golalizadeh", 15.0f, 40);
+        printAndCalculate(zeitarbeiter);
 
         Angestellter angestellter = new Angestellter("Marcel", "Ebert", 3000.0f, 20);
         printAndCalculate(angestellter);
 
-        //ATAngstellter atAngestellter = new ATAngstellter("Kadir", "Erzurum", 2641.52f);
-        //printAndCalculate(atAngestellter);
+        ATAngstellter atAngestellter = new ATAngstellter("Kadir", "Erzurum", 2641.52f);
+        printAndCalculate(atAngestellter);
     }
 
     public static void printAndCalculate(IMitarbeiter IMitarbeiter) {
